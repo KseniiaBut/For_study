5fff18259b34826f1e6bb478048fbced7e09aa2d
KseniiaBut <145913278+KseniiaBut@users.noreply.github.com>
2023-09-24 20:24:32 +0000

    isLeapYear

---
+++ newI.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class newI
+{
+  static int isLeapYear(int y)
+  {
+    if((y % 400 == 0) ||
+       (y % 100 != 0) &&
+       (y % 4 == 0))
+    {
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+ 
+  public static void main(String[] args)
+  {Scanner scan = new Scanner(System.in);
+    System.out.println("Type a year, which I need to check");
+    int answear;
+    answear = scan.nextInt;
+    System.out.println(isLeapYear(answear));
+  }
+}
